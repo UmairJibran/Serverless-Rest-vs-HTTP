9e198282c2ade95321389754c248ca9d080e985d
UmairJibran <umairjibran7@gmail.com>
2022-04-01 11:50:51 +0500

    add functions for rest and http

--- rest-http-perf-test/handler.js
+++ rest-http-perf-test/handler.js
@@ -1,15 +1,19 @@
 'use strict';
 
-module.exports.hello = async (event) => {
+module.exports.rest = async event => {
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v3.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: JSON.stringify({
+      message: 'IT IS REST!',
+    }),
+  };
+};
+
+module.exports.http = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'IT IS HTTP!',
+    }),
   };
 };
